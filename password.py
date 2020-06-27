d449e44f844abe073f5b2935e07fed0395c8bdae
plasticglasses <33756800+plasticglasses@users.noreply.github.com>
2020-06-27 20:53:39 +0100

    Added password and username encryption

--- password.py
+++ password.py
@@ -7,4 +7,14 @@ def save_file(fileName, contents, path):
         myFile.write(contents)
 
 myKey = Fernet.generate_key()
-save_file('key.key', myKey, fileDir)
+save_file('key.key', myKey, fileDIR)
+
+cipher = Fernet(myKey)
+
+username = input("Please input username to encrypt")
+encryptedUsername = cipher.encrypt(username.encode('utf-8')) # bytes
+save_file('user.txt', encryptedUsername, fileDIR+'enc/')
+
+password = input("Please input password to encrypt")
+encryptedPassword = cipher.encrypt(password.encode('utf-8'))
+save_file('pass.txt', encryptedPassword, fileDIR+'enc/')
